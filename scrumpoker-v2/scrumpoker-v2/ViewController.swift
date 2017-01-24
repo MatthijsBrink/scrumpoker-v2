cf3ecc776381ffb22425058421219529b8740684
MatthijsBrink <m.brink@nerds.company>
2017-01-24 08:21:58 +0100

    initial commit

---
+++ scrumpoker-v2/scrumpoker-v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scrumpoker-v2
+//
+//  Created by Stagiare 2 on 24-01-17.
+//  Copyright © 2017 Stagiare 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
